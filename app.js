6ef1e7597972121c6ed3fb6ef77e6e6344231a5d
MelSilgon <112473440+MelSilgon@users.noreply.github.com>
2022-11-13 22:28:59 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,93 @@
+//******** Navbar - fixed scroll ********
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const headerHeight = document.querySelector("header").offsetHeight;
+
+main.style.top = headerHeight + "px";
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  let lastScroll = window.scrollY;
+
+  if (lastScroll > 100) {
+    header.classList.add("scroll-down");
+    header.classList.remove("scroll-up");
+  } else {
+    header.classList.add("scroll-up");
+    header.classList.remove("scroll-down");
+  }
+})
+
+
+//******** The year updates ********
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+//******** Slideshow - automatic and manual ********
+let slideIndex = 1;
+let myTimer;
+let slideshowContainer;
+
+window.addEventListener("load", function () {
+  showSlides(slideIndex);
+  myTimer = setInterval(function () { plusSlides(1) }, 4000);
+
+  //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+
+  //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
+  slideshowContainer.addEventListener('mouseenter', pause)
+  slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n) {
+  clearInterval(myTimer);
+  if (n < 0) {
+    showSlides(slideIndex -= 1);
+  } else {
+    showSlides(slideIndex += 1);
+  }
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  if (n === -1) {
+    myTimer = setInterval(function () { plusSlides(n + 2) }, 4000);
+  } else {
+    myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+  }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+pause = () => {
+  clearInterval(myTimer);
+}
+
+resume = () => {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () { plusSlides(slideIndex) }, 4000);
+}
